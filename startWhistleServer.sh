93e0b047b714d0329c7aaa95f300a00a0427b4e8
Scott Cazan <scottcazan@gmail.com>
2024-09-14 12:43:48 +0200

    rename

--- startWhistleServer.sh
+++ startWhistleServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "whistling mainframe engaged..."
+echo "whistling server engaged..."
 echo "send OSC messages to /whistle followed by a string of comma-separated notes in the form of 'c,e,f#,g' (no spaces)"
 /Applications/SuperCollider.app/Contents/MacOS/sclang ./data/whistler.scd
